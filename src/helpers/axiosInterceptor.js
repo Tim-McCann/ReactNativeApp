21b3f71d6f9a216fc37dbf907ad8b547b6045066
Tim-McCann <tjm8679@gmail.com>
2021-07-05 22:05:59 -0500

    desktop work 7-4... validation work and error messages

--- src/helpers/axiosInterceptor.js
+++ src/helpers/axiosInterceptor.js
@@ -1,31 +1,51 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 import envs from '../config/env';
-
-
-
+// import {LOGOUT} from '../constants/routeNames';
+// import {navigate} from '../navigations/SideMenu/RootNavigator';
 
 let headers = {};
 
 const axiosInstance = axios.create({
-    baseURL: envs.BACKEND_URL,
-    headers,
+  baseURL: envs.BACKEND_URL,
+  headers,
 });
 
+console.log('envs.BACKEND_URL', envs.BACKEND_URL);
+
 axiosInstance.interceptors.request.use(
-    async (config)=>{
-
-        const token = await AsyncStorage.getItem('token');
-        if(token){
-            config.headers.Authorization = 'Bearer ${token}';
-        }
-
-        return config;
-
+    async (config) => {
+      const token = await AsyncStorage.getItem('token');
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
     },
     (error) => {
-        return Promise.reject(error);
+      return Promise.reject(error);
     },
-);
+  );
+
+// axiosInstance.interceptors.response.use(
+//   (response) =>
+//     new Promise((resolve, reject) => {
+//       resolve(response);
+//     }),
+//   (error) => {
+//     if (!error.response) {
+//       return new Promise((resolve, reject) => {
+//         reject(error);
+//       });
+//     }
+
+//     if (error.response.status === 403) {
+//       navigate(LOGOUT, {tokenExpired: true});
+//     } else {
+//       return new Promise((resolve, reject) => {
+//         reject(error);
+//       });
+//     }
+//   },
+// );
 
 export default axiosInstance;
--- src/screens/Register/index.js
+++ src/screens/Register/index.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import { useEffect } from 'react';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { View, Text } from 'react-native';
 import RegisterComponent from '../../components/SignUp';
 import envs from '../../config/env';
+import register from '../../context/actions/auth/register';
+import axios from '../../helpers/axiosInterceptor';
+import {GlobalContext} from '../../context/Provider';
 import axiosInstance from '../../helpers/axiosInterceptor';
+import authInitialState from '../../context/initialStates/authInitialState';
 
 
 const Register = () => {
     const [form, setForm] = useState({});
     const [errors, setErrors] = useState({});
     const {BACKEND_URL} = envs;
+    const {
+      authDispatch, 
+      authState:{error, loading, data},
+    } = useContext(GlobalContext);
 
-    React.useEffect(() => {
-      axiosInstance.post('/contacts').catch(err =>{
-        console.log('err', err.response);
-      });
-    }, []);
 
+    // console.log('BACKEND_URL :>>', BACKEND_URL);
+    // console.log('__DEV__ :>>', __DEV__);
+    // useEffect(() => {
+    //   axiosInstance.post('/contacts').catch((err) => {
+    //     console.log('err', err.response);
+    //   });
+    // }, []);
 
     const onChange = ({name, value}) => {
         setForm({...form, [name]: value});
@@ -47,7 +57,7 @@ const Register = () => {
 
     const onSubmit = () => {
 
-        console.log('form :>> ', form);
+        // console.log('form :>> ', form);
 
         if (!form.userName) {
             setErrors((prev) => {
@@ -74,15 +84,28 @@ const Register = () => {
               return {...prev, password: 'Please add a password'};
             });
           }
+
+          if(
+            Object.values(form).length === 5 &&
+            Object.values(form).every((item) => item.trim().length > 0) &&
+            Object.values(errors).every((item) => !item)
+          ){
+            register(form)(authDispatch);
+            
+
+
+          }
     };
 
     return (
-        <RegisterComponent 
-        onSubmit={onSubmit}  
-        onChange={onChange}
-        form={form} 
-        errors={errors} 
-        />
+      <RegisterComponent
+      onSubmit={onSubmit}
+      onChange={onChange}
+      form={form}
+      errors={errors}
+      error={error}
+      loading={loading}
+    />
     );
 };
 
